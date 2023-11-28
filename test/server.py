8dc3e397719998ced9649ca305df37ad3f2111c5
wwjang <wwjang@korea.ac.kr>
2023-11-28 11:22:08 +0900

    update

---
+++ test/server.py
@@ -0,0 +1,10 @@
+
+
+
+
+'''
+[Reference]
+https://doc.fedml.ai/federate/simulation/examples/sp_fedavg_mnist_lr_example
+https://doc.fedml.ai/federate/simulation/examples/mpi_torch_fedavg_mnist_lr_example
+https://doc.fedml.ai/federate/cross-silo/example/mqtt_s3_fedavg_hierarchical_mnist_lr_example
+'''
