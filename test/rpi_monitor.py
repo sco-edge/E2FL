81cfc433df26d20ce533e36ff3954743b0c04787
wwjang <wwjang@korea.ac.kr>
2023-11-30 11:28:31 +0900

    Update rpi_monitor.py

--- test/rpi_monitor.py
+++ test/rpi_monitor.py
@@ -1,6 +1,12 @@
 import serial
 import time
-
+import timeit
+import logging
+logging.basicConfig(
+    format = '%(asctime)s:%(levelname)s:%(message)s',
+    datefmt = '%m\%d\%Y %I:%M:%S %p',
+    level = logging.DEBUG
+)
 
 def execute_shell_script(serial_port, script_path):
     ser = serial.Serial(serial_port, baudrate=9600, timeout=1)
@@ -20,6 +26,7 @@ def execute_shell_script(serial_port, script_path):
         ser.close()
         
 
+
 if __name__ == "__main__":
     # Configuration
     rpi_serial_port = "/dev/ttyUSB0"
@@ -27,4 +34,37 @@ if __name__ == "__main__":
     
     execute_shell_script(rpi_serial_port, shell_path)
 
+    # execute server on local edge server
+    # command = f'bash run_server.sh'
 
+
+    # execute client
+    # command = f'bash run_client.sh 1'
+    # command = f'bash run_client.sh 2'
+
+    # execute PyMonsoon
+
+
+
+    # and log time 
+    # start = timeit.default_timer()
+    # stop = timeit.default_timer()
+    # duration = stop - start
+
+
+'''
+[Reference]
+https://stackoverflow.com/questions/5622976/how-do-you-calculate-program-run-time-in-python
+https://doc.fedml.ai/federate/getting_started
+https://doc.fedml.ai/federate/cross-silo/example/mqtt_s3_fedavg_hierarchical_mnist_lr_example
+https://doc.fedml.ai/federate/simulation/examples/mpi_torch_fedavg_mnist_lr_example
+
+https://aws.amazon.com/ko/blogs/machine-learning/part-1-federated-learning-on-aws-with-fedml-health-analytics-without-sharing-sensitive-data/
+https://aws.amazon.com/ko/blogs/machine-learning/part-2-federated-learning-on-aws-with-fedml-health-analytics-without-sharing-sensitive-data/
+
+https://github.com/usc-sail/fed-multimodal
+
+https://github.com/FedML-AI/FedML/blob/master/python/fedml/core/distributed/communication/message.py
+https://github.com/FedML-AI/FedML/blob/master/python/fedml/cross_silo/client/fedml_client_master_manager.py#L18
+
+'''
