cb619d535c06895fc989ca6a174e08c46a8fe8b9
wwjang <wwjang@korea.ac.kr>
2023-12-13 16:59:13 +0900

    Update PyMon.py

--- test/power/PyMon.py
+++ test/power/PyMon.py
@@ -7,23 +7,20 @@ import datetime
 pip install monsoon
 '''
 class MyMon():
+    
     def __init__(self, mode, node, vout, channel):
         self.mode = mode
         self.node = node
         self.vout = vout
         self.channel = channel
         if mode == "PyMonsoon":
-            init_run_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            numSamples = 5000 # sample for one second
-
             Mon = LVPM.Monsoon()
             Mon.setup_usb()
 
             Mon.setVout(vout) # vout = 5.5V
-            engine = sampleEngine.SampleEngine(Mon)
-            engine.enableCSVOutput("E2FL_"+init_run_timestamp+".csv")
-            engine.ConsoleOutput(True)
-            engine.startSampling(numSamples)
+            self.engine = sampleEngine.SampleEngine(Mon)
+            self.engine.ConsoleOutput(True)
+            #self.engine.startSampling(numSamples)
 
             # The main power regulator can source 3.0 A of continuous current and 4.5 A of peak current
             # Power up with no current limit for 20 ms, run continuously with the current limit set to 4.6 A
@@ -32,55 +29,39 @@ class MyMon():
             # If it is necessary to vary voltage continuously, without ending the sampling run.
             if vout >= 4.6 and vout <= 5.5:
                 #Disable Main channels
-                engine.disableChannel(sampleEngine.channels.MainCurrent)
-                engine.disableChannel(sampleEngine.channels.MainVoltage)
+                self.engine.disableChannel(sampleEngine.channels.MainCurrent)
+                self.engine.disableChannel(sampleEngine.channels.MainVoltage)
 
                 #Enable USB channels
-                engine.enableChannel(sampleEngine.channels.USBCurrent)
-                engine.enableChannel(sampleEngine.channels.USBVoltage)
+                self.engine.enableChannel(sampleEngine.channels.USBCurrent)
+                self.engine.enableChannel(sampleEngine.channels.USBVoltage)
 
                 #Enable AUX channels
-                engine.enableChannel(sampleEngine.channels.AuxCurrent)
+                self.engine.enableChannel(sampleEngine.channels.AuxCurrent)
 
                 #Set USB Pasthrough mode to 'on', since it defaults to 'auto' 
                 #and will turn off when sampling mode begins
-                Mon.setUSBPassthroughMode(op.USB_Passthrough.On)
+                Mon.setUSBPassthroughMode(op.USB_Passthrough.On) # == 1
             elif vout < 4.6:
                 #Disable USB channels
-                engine.disableChannel(sampleEngine.channels.USBCurrent)
-                engine.disableChannel(sampleEngine.channels.USBVoltage)
+                self.engine.disableChannel(sampleEngine.channels.USBCurrent)
+                self.engine.disableChannel(sampleEngine.channels.USBVoltage)
 
                 #Disable AUX channels
-                engine.disableChannel(sampleEngine.channels.AuxCurrent)
+                self.engine.disableChannel(sampleEngine.channels.AuxCurrent)
 
                 #Enable Main channels
-                engine.enableChannel(sampleEngine.channels.MainCurrent)
-                engine.enableChannel(sampleEngine.channels.Mainoltage)
+                self.engine.enableChannel(sampleEngine.channels.MainCurrent)
+                self.engine.enableChannel(sampleEngine.channels.Mainoltage)
                 
                 #Set USB Pasthrough mode to 'auto' as default
-                Mon.setUSBPassthroughMode(op.USB_Passthrough.Auto)
+                Mon.setUSBPassthroughMode(2) # op.USB_Passthrough.Auto? == 2
             else:
                 raise Exception("The required voltage is not supported on Monsoon Power Monitor.")
-
-            #Don't stop based on sample count, continue until 
-            #the trigger conditions have been satisfied.
-            numSamples=sampleEngine.triggers.SAMPLECOUNT_INFINITE
-
-            #Start when we exceed 100 mA
-            engine.setStartTrigger(sampleEngine.triggers.GREATER_THAN,100)
-
-            #Stop when we drop below 10 mA
-            engine.setStopTrigger(sampleEngine.triggers.LESS_THAN,10)
-                    
-            #Start and stop judged by the channel
-            if vout >= 4.6 and vout <= 5.5: # AUX channel
-                engine.setTriggerChannel(sampleEngine.channels.AUXCurrent)
-            else:  # main channel
-                engine.setTriggerChannel(sampleEngine.channels.MainCurrent)
-
+            
             '''
-            # engine.disableCSVOutput()
-            samples = engine.getSamples()
+            # self.engine.disableCSVOutput()
+            samples = self.engine.getSamples()
 
             #Samples are stored in order, indexed sampleEngine.channels values
             for i in range(len(samples[sampleEngine.channels.timeStamp])):
@@ -88,6 +69,48 @@ class MyMon():
                 Current = samples[sampleEngine.channels.timeStamp][i]
                 print("Main current at time " + repr(timeStamp) + " is: " + repr(Current) + "mA")
             '''
-    def 
+
+    def set_trigger(self, bool, numSamples = 5000):
+        if bool: # trigger mode
+            #Don't stop based on sample count, continue until 
+            #the trigger conditions have been satisfied.
+            numSamples = sampleEngine.triggers.SAMPLECOUNT_INFINITE
+
+            #Start when we exceed 100 mA
+            self.engine.setStartTrigger(sampleEngine.triggers.GREATER_THAN,100)
+
+            #Stop when we drop below 10 mA
+            self.engine.setStopTrigger(sampleEngine.triggers.LESS_THAN,10)
+                    
+            #Start and stop judged by the channel
+            if self.vout >= 4.6 and self.vout <= 5.5: # AUX channel
+                self.engine.setTriggerChannel(sampleEngine.channels.AUXCurrent)
+            else:  # main channel
+                self.engine.setTriggerChannel(sampleEngine.channels.MainCurrent)
+            self.engine.startSampling(numSamples)
+        else: # turn off the trigger mode and start the sampling mode
+            self.engine.startSampling(numSamples)
+
+    def setCSVOutput(self, bool):
+        if bool:
+            init_run_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            self.engine.enableCSVOutput("E2FL_"+init_run_timestamp+".csv")
+        else:
+            self.engine.disableCSVOutput()
+
+    def getSamples(self):
+        return self.engine.getSamples()
+
+    def startSampling(self, numSamples = 5000):
+        # numSamples = sample for one second
+        self.engine.startSampling(numSamples)
+
+    def stopSampling(self):
+        self.engine.stopSampling()
+
+    #def calibration(self)
+    
+
+
             
 
