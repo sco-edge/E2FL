181cc621203ea7ca20dacdf970fff5247e9ff949
wwjang <wwjang@korea.ac.kr>
2023-12-18 18:45:14 +0900

    Update ClientTimer.py

--- log/ClientTimer.py
+++ log/ClientTimer.py
@@ -1,2 +1,16 @@
 import time
 import logging
+
+
+class Timer():
+    '''
+    Time logging for FL and energy monitoring.
+    '''
+
+    def __init__(self, edgeDev_name):
+        self.edgeDev_name = edgeDev_name
+        self.log_entries = []
+
+
+    def startTimer(self):
+        self.
