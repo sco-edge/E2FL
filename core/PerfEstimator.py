ea82cb641fcd4512fbd16ebf8c10740f0a1bea95
wwjang <wwjang@korea.ac.kr>
2023-12-18 16:51:38 +0900

    Update PerfEstimator.py

--- core/PerfEstimator.py
+++ core/PerfEstimator.py
@@ -1,1 +1,23 @@
+from dataclasses import dataclass
 import numpy as np
+
+'''
+Collects RSSI and CSI from edge devices. 
+Estimates conditions of wireless environment from the measurements.
+Predicts the federated learning performance (energy consumption, learning time, so on).
+'''
+
+@dataclass #https://www.daleseo.com/python-dataclasses/
+class WirlENV:
+    '''
+    Those measurements are collected on a per-packet basis.
+    Pkt means 
+    
+    '''
+    Pkt: int
+    RSSI: float
+    CSI: np.complex
+
+class PerfEST():
+    def __init__(self, RSSI, CSI, )
+        
