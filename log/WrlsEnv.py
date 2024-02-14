c8efe0eab0098908cd20f907f2f4e60756db2173
wwjang <wwjang@korea.ac.kr>
2024-02-14 18:48:05 +0900

    Create WrlsEnv.py

---
+++ log/WrlsEnv.py
@@ -0,0 +1,20 @@
+from wifi import Cell, Scheme
+
+
+
+class Timer():
+    '''
+    Time logging for FL and energy monitoring.
+    '''
+
+    def __init__(self, edgeDev_name):
+        self.edgeDev_name = edgeDev_name
+        self.log_entries = []
+
+    def log_event(self, event_name):
+        timestamp = datetime.datetime.now()
+        log_entry = {'device_name': self.edgeDev_name, 'event_name': event_name, 'timestamp': timestamp}
+        self.log_entries.append(log_entry)
+    
+    def get_log_entries(self):
+        return self.log_entries
