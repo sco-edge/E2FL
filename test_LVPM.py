9cac0a406ebd77be54c558a8e07fd3647fc9c0a2
wwjang <wwjang@korea.ac.kr>
2024-04-08 15:16:24 +0900

    Update test_LVPM.py

--- test_LVPM.py
+++ test_LVPM.py
@@ -2,7 +2,7 @@ import usb.core
 
 def device_matcher(d):
     try:
-        return d.idVendor == 0x2AB9 and d.idProduct == 0x0001 and (serialno is None or d.serial_number == str(serialno))
+        return d.idVendor == 0x2AB9 and d.idProduct == 0x0001
     except:#Catches some platform-specific errors when connecting to multiple PMs simultaneously.
         return False
 DEVICE = usb.core.find(custom_match=device_matcher)
