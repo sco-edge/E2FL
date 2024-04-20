264ac38b7bbf9fdca56286d3988b6e4bddf466a6
wwjang <wwjang@korea.ac.kr>
2024-04-20 22:53:23 +0900

    update

--- test_wrls-power.py
+++ test_wrls-power.py
@@ -1,3 +1,6 @@
 from power import Monitor
 from log import WrlsEnv
+import subprocess
+import re
 
+result = subprocess.run(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
