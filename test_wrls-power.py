31b742e1d254b7483c97002f5567d7f0a66cbb57
wwjang <wwjang@korea.ac.kr>
2024-04-20 23:11:03 +0900

    update

--- test_wrls-power.py
+++ test_wrls-power.py
@@ -3,6 +3,18 @@ from log import WrlsEnv
 import subprocess
 import re
 
-def change_WiFi_interface():
+'''
+Wi-Fi interface table
+'Wi-Fi AP': IPTIME AX2002-Mesh
+- b, g, n, ax 20MHz, Korea
+- TX power = 100
+- Beacon 100ms
+
+'AX201'
+'br
+
+'''
+
+def change_WiFi_interface(interf = 'wlan0', channel = 11, rate = '11M', txpower = 15):
     # change Wi-Fi interface 
-    result = subprocess.run(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+    result = subprocess.run([f"iwconfig {interf} channel {channel} rate {rate} txpower {txpower}"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
