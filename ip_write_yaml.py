39f75a857ec148d307dd66339b989db0c029f89b
wwjang <wwjang@korea.ac.kr>
2024-04-30 12:16:41 +0900

    20240430

--- ip_write_yaml.py
+++ ip_write_yaml.py
@@ -3,16 +3,20 @@ import yaml
 # 설정할 데이터 구조
 data = {
     'server': {
-        'host': '192.168.0.17'
+        'host': '192.168.0.17',
+        'ssh_key': 'server.key'
     },
     'RPi3B+': {
-        'host': '192.168.0.14'
+        'host': '192.168.0.14',
+        'ssh_key': 'rpi3b_plus.key'
     },
     'RPi4B': {
-        'host': '192.168.0.15'
+        'host': '192.168.0.15',
+        'ssh_key': 'rpi4b.key'
     },
     'RPi5': {
-        'host': '192.168.0.19'
+        'host': '192.168.0.19',
+        'ssh_key': 'rpi5.key'
     }
 }
 
--- test_wrls-power.py
+++ test_wrls-power.py
@@ -11,8 +11,213 @@ import socket
 import paramiko
 import yaml
 
-logging.basicCOnfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # logging.debug, logging.info, logging.warning, logging.error, logging.critical
+class CustomFormatter(logging.Formatter):
+    
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(levelname)s - %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+def get_ip_address():
+    try:
+        hostname = socket.gethostname()
+        ip_addr = socket.gethostbyname(hostname)
+        return ip_addr
+    except socket.error as e:
+        print(f'Unable to get IP address: {e}')
+        return None
+
+def change_WiFi_interface(interf = 'wlan0', channel = 11, rate = '11M', txpower = 15):
+    # change Wi-Fi interface 
+    result = subprocess.run([f"iwconfig {interf} channel {channel} rate {rate} txpower {txpower}"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+
+
+def start_iperf3_server(server_ip, port=22):
+    """서버 A에서 iperf3 서버를 비동기적으로 시작합니다."""
+    # iperf3 서버 시작 (포트 5201에서 대기)
+    iperf_time_interv = 2
+
+    iperf_client = iperf3.Client()
+    iperf_client.server.hostname = '192.168.0.1'
+    iperf_client.port = 5201
+    iperf_client.duration = 60
+    iperf_client.bind_address = '192.168.0.1' # wi-fi interface's ip address
+
+    return subprocess.Popen(['iperf3', '-s'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def run_iperf3_client(client_SSH, server_ip):
+    """기기 B에서 iperf3 클라이언트를 실행하여 서버 A로 데이터를 전송합니다."""
+    try:
+        # iperf3 클라이언트 실행 명령
+        command = f'iperf3 -c {server_ip}'
+        stdin, stdout, stderr = client_SSH.exec_command(command)
+        
+        # 실행 결과 출력
+        for line in stdout.read().splitlines():
+            logger.info('client_SSH:')
+            logger.info(line.decode('utf-8'))
+        for line in stderr.read().splitlines():
+            logger.error(line.decode('utf-8'))
+    except:
+        logger.error("run_iperf3_client failed.")
+        exit(1)
+
+# 메인 실행
+if __name__ == '__main__':
+    # iperf3 서버를 시작
+    server_process = start_iperf3_server()
+    print("iperf3 서버가 시작되었습니다.")
+
+    # 기기 B의 SSH 접속 정보
+    host = '기기 B의 IP 주소'
+    username = '기기 B의 사용자 이름'
+    key_path = '기기 B의 SSH 키 경로'
+
+    # 클라이언트 실행에 앞서 서버가 시작될 시간을 줍니다.
+    time.sleep(5)
+    
+    # 기기 B에서 iperf3 클라이언트 실행
+    run_iperf3_client(host, username, key_path)
+
+    # 서버 프로세스 종료
+    server_process.terminate()
+    print("iperf3 서버가 종료되었습니다.")
+
+# default parameters
+root_path = './'
+node_A_name = 'rpi3B+'
+node_A_mode = "PyMonsoon"
+
+# Set up logger
+logger = logging.getLogger("test")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
+
+# Set up Power Monitor
+node_A_vout = 5.0
+node_A_triggerBool = True
+node_A_numSamples = 5000
+node_A_thld_high = 100
+node_A_thld_low = 10
+node_A_CSVbool = False #True
+node_A_CSVname = "default"
+rpi3B = Monitor.PowerMon(   node = node_A_name,
+                            vout = node_A_vout,
+                            mode = node_A_mode)
+'''
+    rpi3B.setTrigger(   bool = node_A_triggerBool,
+                        numSamples = node_A_numSamples,
+                        thld_high = node_A_thld_high,
+                        thld_low = node_A_thld_low )
+'''
+rpi3B.setCSVOutput( bool = node_A_CSVbool,
+                    filename = node_A_CSVname)
+
+
+# Get IP address
+# YAML 파일 열기 및 읽기
+with open(root_path+'config.yaml', 'r') as file:
+    config = yaml.safe_load(file)  # 파일에서 YAML을 읽고 파이썬 데이터 구조로 변환
+
+# IP 주소 추출
+server_ip = config['server']['host']  # 'server' 키 아래 'host' 키의 값을 추출
+client_ip = config['RPi3B+']['host']
+
+if server_ip:
+    print("The IP address of the server is: ",server_ip)
+else:
+    print("IP address could not be determined")
+    exit(1)
+
+# Set up SSH service
+client_SSH = paramiko.SSHClient()
+client_SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 호스트 키 자동 추가
+ssh_port = 22
+private_key_path = root_path + config['RPi3B+']['ssh_key']
+client_id = 'pi'
+
+try:
+    mykey = paramiko.RSAKey.from_private_key_file(private_key_path)
+    # SSH 연결
+    client_SSH.connect(client_ip, ssh_port, client_id, pkey=mykey)
+except:
+    logger.error("SSH is failed.")
+    exit()
+
+# Set up iperf3
+time_records = []
+
+
+# Set up WiFi interface
+# 1. Identify the capabilities of the Wi-Fi interface of the currently running system.
+WiFi_rates = [1, 2, 5.5, 11, 6, 9, 12, 18, 24, 36, 48, 54]
+
+# 2. Set the rate (protocl version) of the Wi-Fi interface from the low data rate.
+for rate in WiFi_rates:
+    # Log the start time.
+    time_records.append([f'Wi-Fi start(rate: {rate})',time.time()])
+
+    # Start power monitoring.
+
+    
+    rpi3B.startSampling()
+    samples = rpi3B.getSamples()
+
+
+    # Use iperf3 to measure the Wi-Fi interface's power consumption.
+    iperf_result = iperf_client.run()
+
+    if iperf_result.error:
+        logging.error(iperf_result.error)
+    else:
+        logging.info("iperf3 is done.")
+
+    # End power monitoring.
+
+
+    # Log the end time.
+    time_records.append(['Wi-Fi end(rate: , )',time.time()])
+
+# SSH 연결 종료
+client_SSH.close()
+
+# Calculate each rate's average power consumption.
+current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+filename = f"data_{current_time}.txt"
+try:
+    # 파일을 열려고 시도합니다.
+    f = open(filename, 'w')
+    f.write(time_records)
+    f.close()
+except OSError as e:
+    # OSError 발생 시 오류 코드와 메시지를 출력합니다.
+    logger.error(f"Error opening {filename}: {os.strerror(e.errno)}")
+
+
+# Plot the result.
+
+
+
 
 '''
 # Client-Server Architecture
@@ -56,140 +261,8 @@ data rate
 - $ ssh-keygen
 - $ rpi3b_plus
 - $ # just enter; empty passphase 
-'''
 
-def get_ip_address():
-    try:
-        hostname = socket.gethostname()
-        ip_addr = socket.gethostbyname(hostname)
-        return ip_addr
-    except socket.error as e:
-        print(f'Unable to get IP address: {e}')
-        return None
-
-def change_WiFi_interface(interf = 'wlan0', channel = 11, rate = '11M', txpower = 15):
-    # change Wi-Fi interface 
-    result = subprocess.run([f"iwconfig {interf} channel {channel} rate {rate} txpower {txpower}"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
-
-
-def start_iperf3_server():
-    """서버 A에서 iperf3 서버를 비동기적으로 시작합니다."""
-    # iperf3 서버 시작 (포트 5201에서 대기)
-    return subprocess.Popen(['iperf3', '-s'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-def run_iperf3_client(host, username, key_path):
-    """기기 B에서 iperf3 클라이언트를 실행하여 서버 A로 데이터를 전송합니다."""
-    # SSH 클라이언트 설정
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username=username, key_filename=key_path)
-    
-    try:
-        # 서버 A의 IP 주소 (외부 접근 가능한 IP)
-        server_ip = '서버 A의 IP 주소 입력'
-        # iperf3 클라이언트 실행 명령
-        command = f'iperf3 -c {server_ip}'
-        stdin, stdout, stderr = client.exec_command(command)
-        
-        # 실행 결과 출력
-        for line in stdout:
-            print('STDOUT:', line.strip())
-        for line in stderr:
-            print('STDERR:', line.strip())
-            
-    finally:
-        client.close()
-
-# 메인 실행
-if __name__ == '__main__':
-    # iperf3 서버를 시작
-    server_process = start_iperf3_server()
-    print("iperf3 서버가 시작되었습니다.")
-
-    # 기기 B의 SSH 접속 정보
-    host = '기기 B의 IP 주소'
-    username = '기기 B의 사용자 이름'
-    key_path = '기기 B의 SSH 키 경로'
-
-    # 클라이언트 실행에 앞서 서버가 시작될 시간을 줍니다.
-    time.sleep(5)
-    
-    # 기기 B에서 iperf3 클라이언트 실행
-    run_iperf3_client(host, username, key_path)
-
-    # 서버 프로세스 종료
-    server_process.terminate()
-    print("iperf3 서버가 종료되었습니다.")
-
-# Set up Power Monitor
-node_A_name = 'rpi3B+'
-node_A_vout = 5.0
-node_A_mode = "PyMonsoon"
-node_A_triggerBool = True
-node_A_numSamples = 5000
-node_A_thld_high = 100
-node_A_thld_low = 10
-node_A_CSVbool = False#True
-node_A_CSVname = "default"
-rpi3B = Monitor.PowerMon(   node = node_A_name,
-                            vout = node_A_vout,
-                            mode = node_A_mode)
-'''
-    rpi3B.setTrigger(   bool = node_A_triggerBool,
-                        numSamples = node_A_numSamples,
-                        thld_high = node_A_thld_high,
-                        thld_low = node_A_thld_low )
-'''
-rpi3B.setCSVOutput( bool = node_A_CSVbool,
-                    filename = node_A_CSVname)
-
-
-# Get IP address
-# YAML 파일 열기 및 읽기
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)  # 파일에서 YAML을 읽고 파이썬 데이터 구조로 변환
-
-# IP 주소 추출
-server_ip = config['server']['host']  # 'server' 키 아래 'host' 키의 값을 추출
-client_ip = config['RPi3B+']['host']
-
-if server_ip:
-    print("The IP address of the server is: ",server_ip)
-else:
-    print("IP address could not be determined")
-    exit(1)
-
-# Set up SSH service
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 호스트 키 자동 추가
-ssh_port = 22
-private_key_path = './rpi3b_plus.key'
-client_id = 'pi'
-
-try:
-    mykey = paramiko.RSAKey.from_private_key_file(private_key_path)
-    # SSH 연결
-    client.connect(client_ip, ssh_port, client_id, pkey=mykey)
-
-    # iperf3 명령 실행 (서버 A로 데이터를 보내는 클라이언트로 기기 B를 설정)
-    command = f'iperf3 -c {server_ip}'
-    stdin, stdout, stderr = client.exec_command(command)
-    
-    # 결과 출력
-    print("STDOUT:")
-    for line in stdout.read().splitlines():
-        print(line.decode('utf-8'))
-    print("\nSTDERR:")
-    for line in stderr.read().splitlines():
-        print(line.decode('utf-8'))
-
-finally:
-    # SSH 연결 종료
-    client.close()
-
-
-# Set up iperf3
-'''
+# iperf3
     iperf3
 
     -s : server mode
@@ -209,60 +282,3 @@ finally:
     -P #: number of parallel client thrads to run
     -T : time-to-live, for multicast (default 1)
 '''
-iperf_time_interv = 2
-
-iperf_client = iperf3.Client()
-iperf_client.server.hostname = '192.168.0.1'
-iperf_client.port = 5201
-iperf_client.duration = 60
-iperf_client.bind_address = '192.168.0.1' # wi-fi interface's ip address
-
-
-time_records = []
-
-
-# Set up WiFi interface
-# 1. Identify the capabilities of the Wi-Fi interface of the currently running system.
-WiFi_rates = [1, 2, 5.5, 11, 6, 9, 12, 18, 24, 36, 48, 54]
-
-# 2. Set the rate (protocl version) of the Wi-Fi interface from the low data rate.
-for rate in WiFi_rates:
-    # Log the start time.
-    time_records.append([f'Wi-Fi start(rate: {rate})',time.time()])
-
-    # Start power monitoring.
-
-    
-    rpi3B.startSampling()
-    samples = rpi3B.getSamples()
-
-
-    # Use iperf3 to measure the Wi-Fi interface's power consumption.
-    iperf_result = iperf_client.run()
-
-    if iperf_result.error:
-        logging.error(iperf_result.error)
-    else:
-        print("iperf3 is done.")
-
-    # End power monitoring.
-
-
-    # Log the end time.
-    time_records.append(['Wi-Fi end(rate: , )',time.time()])
-
-
-# Calculate each rate's average power consumption.
-current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-filename = f"data_{current_time}.txt"
-try:
-    # 파일을 열려고 시도합니다.
-    f = open(filename, 'w')
-    f.write(time_records)
-    f.close()
-except OSError as e:
-    # OSError 발생 시 오류 코드와 메시지를 출력합니다.
-    print(f"Error opening {filename}: {os.strerror(e.errno)}")
-
-
-# Plot the result.
