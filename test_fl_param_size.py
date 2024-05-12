1f10a27c2357138ea3b72a887eeae61add42c1d7
wwjang <wwjang@korea.ac.kr>
2024-05-12 20:36:47 +0900

    Update test_fl_param_size.py

--- test_fl_param_size.py
+++ test_fl_param_size.py
@@ -2,3 +2,4 @@
 client_cmd = "python3.10 ./FLOWER_embedded_devices/client_pytorch.py --cid=$client_id --server_address=$server_address --mnist"
 server_cmd = "python3.10 ./FLOWER_embedded_devices/server.py --rounds $round --min_num_clients $num_clients --sample_fraction $sample_frac"
 
+# check packet size
