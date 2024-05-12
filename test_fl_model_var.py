07e3b3372e96636eae6e9e8af2e922fcf6cf61f4
wwjang <wwjang@korea.ac.kr>
2024-05-12 20:38:20 +0900

    Create test_fl_model_var.py

---
+++ test_fl_model_var.py
@@ -0,0 +1,5 @@
+
+FL_model = []
+
+client_cmd = "python3.10 ./FLOWER_embedded_devices/client_pytorch.py --cid=$client_id --server_address=$server_address --mnist"
+server_cmd = "python3.10 ./FLOWER_embedded_devices/server.py --rounds $round --min_num_clients $num_clients --sample_fraction $sample_frac"
