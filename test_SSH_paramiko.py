ae8d4807f2600e915dfc68d14e1be244784ed506
wwjang <wwjang@korea.ac.kr>
2024-05-20 15:39:08 +0900

    Update test_SSH_paramiko.py

--- test_SSH_paramiko.py
+++ test_SSH_paramiko.py
@@ -78,7 +78,7 @@ mykey = paramiko.RSAKey.from_private_key_file(private_key_path)
 
 try_count = 0
 try:
-    client_SSH.connect(hostname = client_ip, port = ssh_port, username = client_ssh_id, passphrase="", key_filename=private_key_path, look_for_keys=False)
+    client_SSH.connect(hostname = client_ip, port = ssh_port, username = client_ssh_id, passphrase="", pkey=mykey, look_for_keys=False)
     print("SUCCESS")
 except Exception as e:
     logger.error("SSH is failed: ", e)
