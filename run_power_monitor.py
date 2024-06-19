80288130b64d299fad4c6e7fef6fd3a690fbb231
wwjang <wwjang@korea.ac.kr>
2024-06-19 15:41:02 +0900

    20240619

--- run_power_monitor.py
+++ run_power_monitor.py
@@ -1,3 +1,5 @@
+from power import Monitor
+import Monsoon.sampleEngine as sampleEngine
 from log import WrlsEnv
 from datetime import datetime
 import subprocess, os, logging, time, socket, pickle
@@ -19,31 +21,6 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-parser = argparse.ArgumentParser(description="Flower Embedded devices")
-parser.add_argument(
-    "--server_address",
-    type=str,
-    default="0.0.0.0:8080",
-    help=f"gRPC server address (deafault '0.0.0.0:8080')",
-)
-parser.add_argument(
-    "--rounds",
-    type=int,
-    default=5,
-    help="Number of rounds of federated learning (default: 5)",
-)
-parser.add_argument(
-    "--sample_fraction",
-    type=float,
-    default=1.0,
-    help="Fraction of available clients used for fit/evaluate (default: 1.0)",
-)
-parser.add_argument(
-    "--min_num_clients",
-    type=int,
-    default=2,
-    help="Minimum number of available clients required for sampling (default: 2)",
-)
 _UPTIME_RPI3B = 500
 
 # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -79,27 +56,6 @@ def get_ip_address():
         print(f'Unable to get IP address: {e}')
         return None
 
-def change_WiFi_interface(interf = 'wlan0', channel = 11, rate = '11M', txpower = 15):
-    # change Wi-Fi interface 
-    result = subprocess.run([f"iwconfig {interf} channel {channel} rate {rate} txpower {txpower}"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
-    return result
-
-def change_WiFi_interface_client(client_ssh, interf = 'wlan0', channel = 11, rate = '11M', txpower = 15):
-    # change Wi-Fi interface 
-    stdin, stdout, stderr = client_ssh.send(f"iwconfig {interf} channel {channel} rate {rate} txpower {txpower}") # exec_command
-    time.sleep(2)
-    
-    # Receive the output
-    output = client_ssh.recv(65535).decode() # 65535 is the maximum bytes that can read by recv() method.
-
-    if 'error' in output.lower() or 'command not found' in output.lower():
-            logger.error("Error detected in the command output.")
-            return False
-    else:
-        logger.info("iwconfig executed successfully.")
-        logger.info(output)
-    
-    return True
 
 def get_client_SSH(client_ip, wait_time):
     # Wait for boot up
@@ -143,58 +99,16 @@ def get_client_SSH(client_ip, wait_time):
 
     return client_shell
 
-# FL; define metric aggregation function
-def weighted_average(metrics: List[Tuple[int, Metrics]]):
-    """This function averages teh `accuracy` metric sent by the clients in a `evaluate`
-    stage (i.e. clients received the global model and evaluate it on their local
-    validation sets)."""
-    # Multiply accuracy of each client by number of examples used
-    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
-    examples = [num_examples for num_examples, _ in metrics]
-
-    # Aggregate and return custom metric (weighted average)
-    return {"accuracy": sum(accuracies) / sum(examples)}
-
-
-def fit_config(server_round: int):
-    """Return a configuration with static batch size and (local) epochs."""
-    config = {
-        "epochs": 3,  # Number of local epochs done by clients
-        "batch_size": 16,  # Batch size to use by clients during fit()
-    }
-    return config
-
-
-def fl_server(server_address, num_clients=4, sample_frac=1.0, round=3):
-    # Define strategy
-    strategy = fl.server.strategy.FedAvg(
-        server_address = server_address,
-        fraction_fit=sample_frac,
-        fraction_evaluate=sample_frac,
-        min_fit_clients=num_clients,
-        on_fit_config_fn=fit_config,
-        evaluate_metrics_aggregation_fn=weighted_average,
-    )
-    # Start Flower server
-    fl.server.start_server(
-        server_address=server_address,
-        config=fl.server.ServerConfig(num_rounds=round),
-        strategy=strategy,
-    )
-
 # default parameters
 root_path = os.path.abspath(os.getcwd())+'/'
 node_A_name = 'rpi3B+'
 node_A_mode = "PyMonsoon"
 client_ssh_id = 'pi'
 ssh_port = 22
+
 args = parser.parse_args()
 print(args)
 
-# FL parameters
-FL_num_clients = 4
-FL_sample_frac = 1.0
-FL_round = 3
 
 # Set up logger
 logger = logging.getLogger("test")
@@ -203,6 +117,24 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 ch.setFormatter(CustomFormatter())
 logger.addHandler(ch)
+
+# Set up Power Monitor
+node_A_vout = 5.0
+node_A_triggerBool = True
+node_A_numSamples = sampleEngine.triggers.SAMPLECOUNT_INFINITE
+node_A_thld_high = 100
+node_A_thld_low = 10
+node_A_CSVbool = False #True
+node_A_CSVname = "default"
+rpi3B = Monitor.PowerMon(   node = node_A_name,
+                            vout = node_A_vout,
+                            mode = node_A_mode,
+                            ConsoleIO = False)
+rpi3B.setTrigger(   bool = node_A_triggerBool,
+                    thld_high = node_A_thld_high,
+                    thld_low = node_A_thld_low )
+rpi3B.setCSVOutput( bool = node_A_CSVbool,
+                    filename = node_A_CSVname)
 
 # Read the YAML config file.
 with open(root_path+'config.yaml', 'r') as file:
@@ -227,9 +159,9 @@ else:
 
 
 # set client_SSH
-#client_shells = []
-#for c_ip in [client_ip1, client_ip2, client_ip3, client_ip4]:
-#    client_shells.append(get_client_SSH(client_ip = c_ip, wait_time = _UPTIME_RPI3B))
+client_shells = []
+for c_ip in [client_ip1, client_ip2, client_ip3, client_ip4]:
+    client_shells.append(get_client_SSH(client_ip = c_ip, wait_time = _UPTIME_RPI3B))
 
 
 # Prepare a bucket to store the results.
@@ -237,34 +169,43 @@ measurements_dict = []
 
 WiFi_rates = [1] #[1, 2, 5.5, 11, 6, 9, 12, 18, 24, 36, 48, 54]
 
-# Start the FL server.
-try:
-    fl_server(server_address=args.server_ip, \
-            num_clients=args.min_num_clients, sample_frac=args.sample_fraction, round=args.rounds)
-    logger.info("Start FL server.")
-    # Wait for server to start fl properly.
-    time.sleep(5)
-except Exception as e:
-    logger.error('FL is failed: ', e)
-    exit(1)
+for rate in WiFi_rates:
+    time_records = []
 
+    # Log the start time.
+    time_records.append(time.time())
 
-# Log the end time.
-#logger.info([f'Wi-Fi end(rate: {rate})',time.time()])
+    input("Press Enter to start power monitoring")
 
-# measurements_dict.append({'rate': rate, 'time': time_records, 'power': samples})
+    # Start power monitoring.
+    rpi3B.startSampling(numSamples = node_A_numSamples) # it will take measurements every 200us
+    logger.info('Start the power monitor.')
+
+    input("Press Enter to stop power monitoring")
+
+    # End power monitoring.
+    rpi3B.stopSampling()
+    samples = rpi3B.getSamples()
+    logger.info('Stop the power monitor.')
+
+    # Log the end time.
+    time_records.append(time.time())
+    logger.info([f'Wi-Fi end(rate: {rate})',time.time()])
+
+    measurements_dict.append({'rate': rate, 'time': time_records, 'power': samples})
 
 # Close the SSH connection.
-#for client_SSH in client_shells:
-#    client_SSH.close()
+for client_SSH in client_shells:
+    client_SSH.close()
 
 
 # Save the data.
-#current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-#filename = f"data_{current_time}.pickle"
-#with open(filename, 'wb') as handle:
-#    pickle.dump(measurements_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#logger.info(f"The measurement data is saved as {filename}.")
+current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+filename = f"data_{current_time}.pickle"
+with open(filename, 'wb') as handle:
+    pickle.dump(measurements_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+logger.info(f"The measurement data is saved as {filename}.")
+
 
 
 
