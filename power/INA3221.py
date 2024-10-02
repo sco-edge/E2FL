2b41faa4cc47682fe8cf0ce2c66ca0e9ae27bdfd
wwjang <wwjang@korea.ac.kr>
2024-10-02 11:36:46 +0900

    Update INA3221.py

--- power/INA3221.py
+++ power/INA3221.py
@@ -1,25 +1,6 @@
 # https://github.com/tgraf/bmon; sysfs
 # https://github.com/zzzDavid/xavier-power-profiling
 
-import time
-import os
-import json
-import pickle
-import torch
-import ofa
-
-from torch.multiprocessing import Process, Queue, set_start_method
-
-try:
-    set_start_method('spawn')
-except RuntimeError:
-    pass
-
-from read_power import read_sysfs
-from build_model import build_block
-from tqdm import tqdm
-from time import sleep
-
 '''
 https://forums.developer.nvidia.com/t/jetson-orin-nx-power-management-parameters/280460
 https://docs.nvidia.com/jetson/archives/r35.4.1/DeveloperGuide/text/SD/PlatformPowerAndPerformance/JetsonOrinNanoSeriesJetsonOrinNxSeriesAndJetsonAgxOrinSeries.html?#jetson-orin-nx-series-and-jetson-orin-nano-series
@@ -72,196 +53,118 @@ in4_label: Sum of shunt voltages
 CPU, GPU, CV? https://developer.nvidia.com/blog/maximizing-deep-learning-performance-on-nvidia-jetson-orin-with-dla/
 
 '''
+import threading
+import time
+import os
+import pickle
 
-# configurations
-times = 1000 # times to run the blocks
+class EnergyMonitor:
+    def __init__(self, sysfs_path='/sys/bus/i2c/drivers/ina3221/1-0040/iio_device/in_power0_input'):
+        """
+        Initialize the EnergyMonitor class
+        :param sysfs_path: Path to the sysfs file for energy data (default path for Jetson devices)
+        """
+        self.sysfs_path = sysfs_path
+        self.freq = 1  # Frequency in seconds (default)
+        self.monitoring = False
+        self.thread = None
+        self.energy_data = []  # List to store energy data (power readings)
+        self.start_time = None
+        self.end_time = None
 
-class read_sysfs:
-    def __init__(self, filename = '/sys/bus/i2c/drivers/ina3221/1-0040/iio_device/in_power0_input'):
-        self.filename = filename
+    def _read_sysfs(self):
+        """
+        Read the power consumption value from sysfs
+        """
+        try:
+            with open(self.sysfs_path, 'r') as f:
+                power = f.read().strip()
+            return float(power)  # Assuming power value is in float format (e.g., mW)
+        except Exception as e:
+            print(f"Error reading sysfs: {e}")
+            return None
 
-    def read_sysfs(self):
-        # sysfs node of GPU power in mW
-        with open(self.filename, 'r') as f:
-            power = f.read();
-        power = power.replace('\n', '')
-        return power
+    def _monitor(self):
+        """
+        Monitor the energy usage in a separate thread
+        """
+        while self.monitoring:
+            power = self._read_sysfs()
+            if power is not None:
+                self.energy_data.append(power)
+            time.sleep(self.freq)
 
-    def read(self):
-        pairs = list()
-        for i in range(5000):
-            start = time.time()
-            power = self.read_sysfs()
-            end = time.time()
-            time_passed = (end - start) * 1e3 # ms
-            pairs.append((power, time_passed))
-        with open('power_time_pairs.pkl', 'wb') as f:
-            pickle.dump(pairs, f, pickle.HIGHEST_PROTOCOL)
-        return pairs
-        
+    def start(self, freq=1):
+        """
+        Start energy monitoring at the specified frequency
+        :param freq: Frequency in seconds to sample energy data
+        """
+        if self.monitoring:
+            print("Energy monitoring is already running.")
+            return
 
-def load_config_list(filename):
-    # read in block configurations
-    with open(filename, 'rb') as f:
-        block_config = pickle.load(f)
-    return block_config
+        self.freq = freq
+        self.monitoring = True
+        self.energy_data = []  # Reset energy data on start
+        self.start_time = time.time()  # Capture start time
+        self.thread = threading.Thread(target=self._monitor)
+        self.thread.start()
+        print(f"Energy monitoring started with a frequency of {self.freq} second(s).")
 
+    def stop(self):
+        """
+        Stop energy monitoring and return the elapsed time and the amount of data collected
+        :return: Elapsed time (seconds), data size (number of power readings)
+        """
+        if not self.monitoring:
+            print("Energy monitoring is not running.")
+            return None, None
 
-def static_power(filename):
-    print("sleeping to stablize system ...")
-    sleep(10)
-    print("measuring static power ...")
+        self.monitoring = False
+        self.thread.join()  # Wait for thread to finish
+        self.end_time = time.time()  # Capture end time
+        elapsed_time = self.end_time - self.start_time
+        data_size = len(self.energy_data)
+        print(f"Energy monitoring stopped. Time elapsed: {elapsed_time:.2f}s, Data size: {data_size}")
+        return elapsed_time, data_size
 
-    static_power = 0
-    for _ in range(1000):
-        power = read_sysfs()
-        static_power += float(power)
-    static_power /= 1000
-    
-    with open(filename, 'w') as f:
-        f.write(str(static_power))
+    def save(self, fpath):
+        """
+        Save the energy data to a file using pickle
+        :param fpath: Path to save the data (file extension can be .pkl or any other format)
+        """
+        if not self.energy_data:
+            print("No energy data to save.")
+            return
 
-    print(f'static power: {static_power}')
+        with open(fpath, 'wb') as f:
+            pickle.dump(self.energy_data, f)
+        print(f"Energy data saved to {fpath}.")
 
-# ------- power measurement thread ---------
-def power_thread(power_q, interval_q, in_q):
-    # t_list = list() # time interval list
-    # p_list = list() # power(mW) list
-    
-    while True:
-        start = time.perf_counter() # time interval in second
+    def close(self):
+        """
+        Gracefully terminate the thread and cleanup resources
+        """
+        if self.monitoring:
+            self.stop()
 
-        power = read_sysfs()
+        print("EnergyMonitor closed.")
 
-        end = time.perf_counter() 
-        interval = end - start
-
-        # p_list.append(power)
-        # t_list.append(interval)
-        
-        interval_q.put(interval)
-        power_q.put(power)
-
-        if not in_q.empty():
-            print("power thread exited")
-            break        
-    return 
-
-# -------- latency measurement thread --------
-_sentinel = object()
-def latency_thread(model_queue, input_tensor_queue, return_q, out_q):
-    """
-    we return a list of latnecy data, becasue I'm not sure if 
-    there would a warm up period when we measure with pytorch
-    """
-    input_tensor = input_tensor_queue.get()
-    model = model_queue.get()
-    model = model.to('cuda')
-    input_tensor = input_tensor.to('cuda')
-
-    # test latency here
-    for _ in range(times):
-        start = time.perf_counter()
-        model(input_tensor)
-        end = time.perf_counter()
-        latency = (end - start)
-        return_q.put(latency)
-    
-    
-    del input_tensor
-
-    out_q.put(_sentinel)
-    print("latency_thread exited")
-    return
-
-
-# launches both latency and power threads
-# to measure dynamic power and latency for one model
-def dynamic_power(model, input_shape):
-    q = Queue()
-    power_return = Queue()
-    interval_return = Queue()
-    latency_return = Queue()
-    input_tensor_queue = Queue()
-    model_queue = Queue()
-    
-    input_tensor = torch.ones([*input_shape])
-    input_tensor_queue.put(input_tensor)
-    
-    model.share_memory()
-
-    model_queue.put(model)
-    
-    context = torch.multiprocessing.get_context('spawn')
-
-    p_thread = context.Process(target=power_thread, args=(power_return, interval_return, q))
-    l_thread = context.Process(target=latency_thread, args=(model_queue, input_tensor_queue, latency_return, q))
-
-    l_thread.start()
-    p_thread.start()
-
-    power_l = list() # GPU power list
-    interval_l = list() # power interval list
-    latency_l = list() # latency list
-
-    l_thread.join()
-
-    while True:
-        if not power_return.empty():
-            power_l.append(power_return.get())
-        if not interval_return.empty():
-            interval_l.append(interval_return.get())
-        if not latency_return.empty():
-            latency_l.append(latency_return.get())
-        if power_return.empty() and interval_return.empty() and latency_return.empty():
-            break
-
-    power_return.close()
-    interval_return.close()
-    latency_return.close()
-    q.close()
-
-    del q
-    del power_return
-    del latency_return
-    del interval_return
-
-    return latency_l, power_l, interval_l 
-
-
-# main function
-def main():
-    # measure static power
-    root_dir = './'
-    static_power_filename = root_dir+'static_power.txt'
-    config_filename = root_dir+'config_list.pkl'
-
-    block_config = load_config_list(config_filename)
-
-    static_power(static_power_filename)
-
-
-    # take care of the result folder
-    result_dir = './results'
-    if not os.path.exists(result_dir):
-        os.makedirs(result_dir)
-        print('made new directory: ' + result_dir)
-
-    # measure dynamic power
-    for idx, b_conf in enumerate(tqdm(block_config)):
-        in_channel = b_conf['mobile_inverted_conv']['in_channels']
-        h, w = b_conf['input_size']
-        model = build_block(b_conf)
-        latency_l, power_l, interval_l = dynamic_power(model, (1, in_channel, h, w))
-        print(f"latency_l length = {len(latency_l)}, power_l length={len(power_l)}, interval_l length={len(interval_l)}")
-        # save each list into a txt file
-        with open(os.path.join(result_dir, str(idx) + '_latency_torch.txt'), 'w') as f:
-            for v in latency_l: f.write(str(v) + '\n')
-        with open(os.path.join(result_dir, str(idx) + '_power.txt'), 'w') as f:
-            for v in power_l: f.write(str(v) + '\n')
-        with open(os.path.join(result_dir, str(idx) + '_interval.txt'), 'w') as f:
-            for v in interval_l: f.write(str(v) + '\n')
-        print("saved results to file.")
-
+# Example usage in any Python code
 if __name__ == "__main__":
-    main()
+    monitor = EnergyMonitor()
+
+    # Start monitoring with a frequency of 2 seconds
+    monitor.start(freq=2)
+
+    # Simulate a task
+    time.sleep(10)
+
+    # Stop monitoring
+    elapsed, data_size = monitor.stop()
+
+    # Save the collected energy data to a file
+    monitor.save('energy_data.pkl')
+
+    # Close the monitor and clean up
+    monitor.close()
