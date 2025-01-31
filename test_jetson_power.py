b9602e7d398242a3b1308b1f63bfe525d5072d23
wwjang <wwjang@korea.ac.kr>
2025-01-31 14:11:59 +0900

    Update test_jetson_power.py

--- test_jetson_power.py
+++ test_jetson_power.py
@@ -2,10 +2,10 @@ import time
 from power.INA3221 import INA3221
 
 # Initialize power monitor
-monitor = INA3221(freq=0.5)  # Sample every 0.5 seconds
+monitor = INA3221()  # Sample every 0.5 seconds
 
 # Start monitoring
-monitor.start()
+monitor.start(freq=0.5)
 print("Power monitoring started. Collecting data for 10 seconds...")
 
 # Let it run for 10 seconds
